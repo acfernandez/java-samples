d887fb0d45b0234336c8ada437d0dfef9965a8d5
Alberto Fernandez <afernandez@despegar.com>
2017-12-22 18:40:07 -0300

    Round calculus

--- src/main/java/ar/com/java/samples/acf/MathSamples.java
+++ src/main/java/ar/com/java/samples/acf/MathSamples.java
@@ -3,18 +3,25 @@ package ar.com.java.samples.acf;
 import static org.springframework.util.Assert.isTrue;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class MathSamples {
 
 	private static final BigDecimal DIVISOR = BigDecimal.valueOf(1000000);
 
-	private BigDecimal calculate(int areaResult, int perfResult, int companyResult, 
-			int areaMultiplicand, int perfMultiplicand) {
+	private BigDecimal calculate(int areaResult, int perfResult, int companyResult, int areaMultiplicand,
+			int perfMultiplicand) {
 
 		BigDecimal area = BigDecimal.valueOf(areaResult * areaMultiplicand);
 		BigDecimal perf = BigDecimal.valueOf(perfResult * perfMultiplicand);
+
 		return (perf == BigDecimal.ZERO) ? BigDecimal.ZERO
-				: area.add(perf).multiply(BigDecimal.valueOf(companyResult)).divide(DIVISOR);
+				: area
+				.add(perf)
+				.multiply(BigDecimal.valueOf(companyResult))
+				.divide(DIVISOR)
+				.round(new MathContext(1, RoundingMode.HALF_UP));
 	}
 
 	private BigDecimal calculateForColaborator(int areaResult, int perfResult, int companyResult) {
@@ -30,19 +37,19 @@ public class MathSamples {
 		MathSamples samples = new MathSamples();
 
 		isTrue("1".equals(samples.calculateForColaborator(100, 100, 100).toString()));
-		isTrue("1.92".equals(samples.calculateForColaborator(120, 120, 160).toString()));
+		isTrue("2".equals(samples.calculateForColaborator(120, 120, 160).toString()));
 		isTrue("0.8".equals(samples.calculateForColaborator(80, 80, 100).toString()));
 		isTrue("0.6".equals(samples.calculateForColaborator(0, 100, 100).toString()));
 		isTrue("0".equals(samples.calculateForColaborator(100, 0, 100).toString()));
 
 		isTrue("1".equals(samples.calculateForManager(100, 100, 100).toString()));
-		isTrue("1.92".equals(samples.calculateForManager(120, 120, 160).toString()));
+		isTrue("2".equals(samples.calculateForManager(120, 120, 160).toString()));
 		isTrue("0.4".equals(samples.calculateForManager(100, 100, 40).toString()));
 		isTrue("0.5".equals(samples.calculateForManager(0, 100, 100).toString()));
 		isTrue("0".equals(samples.calculateForManager(100, 0, 100).toString()));
 
-		BigDecimal salary = BigDecimal.valueOf(1);
-		System.out.println(samples.calculateForManager(0, 0, 0).multiply(salary));
+		BigDecimal salary = BigDecimal.valueOf(25000);
+		System.out.println(samples.calculateForManager(120, 120, 160).multiply(salary));
 	}
 
 }
